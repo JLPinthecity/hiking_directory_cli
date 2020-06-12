ee87a5cf3f68afb1b034e8a877d29d5f719848d3
Joann Pan <joannlpan@gmail.com>
2020-06-12 21:26:21 +0000

    add require relatives

--- lib/hiking_directory.rb
+++ lib/hiking_directory.rb
@@ -1,4 +1,6 @@
-require "hiking_directory/version"
+require_relative "./hiking_directory/version"
+require_relative "./hiking_directory/cli"
+
 
 module HikingDirectory
   class Error < StandardError; end
