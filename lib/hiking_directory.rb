8db533a86ea8915075c490eaf870a818e126b4fd
Joann Pan <joannlpan@gmail.com>
2020-06-23 21:17:58 +0000

    required nokogiri

--- lib/hiking_directory.rb
+++ lib/hiking_directory.rb
@@ -2,7 +2,9 @@ require_relative "./hiking_directory/version"
 require_relative "./hiking_directory/cli"
 require_relative "./hiking_directory/state"
 require_relative "./hikingdirectory/scraper"
+
 require 'pry'
+require 'nokogiri'
 
 module HikingDirectory
   class Error < StandardError; end
