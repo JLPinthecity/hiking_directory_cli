cd3f9e85f4384511fc83e4e20089261dcf8c463d
Joann Pan <joannlpan@gmail.com>
2020-06-26 20:59:27 +0000

    scrape first level

--- lib/hiking_directory/scraper.rb
+++ lib/hiking_directory/scraper.rb
@@ -1,7 +1,15 @@
 class HikingDirectory::Scraper
+  attr_accessor :name, :url 
   
   def self.scrape_states
     doc = Nokogiri::HTML(open("https://www.hikingproject.com/directory/areas"))
+    states_cards = doc.css("div.row.areas div.card.area-card")
+    states_cards.each do |thing|
+    name = thing.css("h3.dont-shrink.serif").text
+    url = thing.css("a").attr("href").value
+    
     binding.pry
+  end
   end 
+  
 end
