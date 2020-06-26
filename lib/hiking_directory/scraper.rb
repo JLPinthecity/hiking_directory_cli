5f735d1b3bba40a21046bd3eb90e34ad9f6db5cc
Joann Pan <joannlpan@gmail.com>
2020-06-26 21:28:35 +0000

    create state instances

--- lib/hiking_directory/scraper.rb
+++ lib/hiking_directory/scraper.rb
@@ -1,12 +1,13 @@
 class HikingDirectory::Scraper
-  attr_accessor :name, :url 
   
   def self.scrape_states
     doc = Nokogiri::HTML(open("https://www.hikingproject.com/directory/areas"))
     states_cards = doc.css("div.row.areas div.card.area-card")
     states_cards.each do |thing|
-    name = thing.css("h3.dont-shrink.serif").text
-    url = thing.css("a").attr("href").value
+      name = thing.css("h3.dont-shrink.serif").text
+      HikingDirectory::State.new(name)
+      url = thing.css("a").attr("href").value
+      
     
     binding.pry
   end
